2e15f218de392ee3f12d8c7805687db326829e74
AndrewKhdot <62639706+AndrewKhdot@users.noreply.github.com>
2022-12-15 14:23:55 +0300

    Update TextAreaComponent.js

--- reactcourse/src/components/TextAreaComponent.js
+++ reactcourse/src/components/TextAreaComponent.js
@@ -3,10 +3,10 @@ import React from 'react';
 class TextAreaComponent extends React.Component {
     render() {
       return(
-        <React.Fragment>
+        <>
             <label htmlFor={'input' + this.props.name}>{this.props.descprition + ':'}</label>
             <textarea placeholder={this.props.descprition} id={'input' + this.props.name}></textarea>
-        </React.Fragment>
+        </>
       )
     }
   }
