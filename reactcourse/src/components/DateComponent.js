2226a277b4dfc342a9ba2141ba2323c27fd0100e
AndrewKhdot <62639706+AndrewKhdot@users.noreply.github.com>
2022-12-15 14:22:07 +0300

    Update DateComponent.js

--- reactcourse/src/components/DateComponent.js
+++ reactcourse/src/components/DateComponent.js
@@ -3,10 +3,10 @@ import React from 'react';
 class DateComponent extends React.Component {
     render() {
       return(
-        <React.Fragment>
+        <>
             <label htmlFor='BirthDate'>{'Дата рождения:'}</label>
             <input type="date" id = 'BirthDate'></input>
-        </React.Fragment>
+        </>
       )
     }
   }
