8676a43e049d1f444b7ad3ac6f09f3b6dddafc71
AndrewKhdot <62639706+AndrewKhdot@users.noreply.github.com>
2022-12-15 14:24:29 +0300

    Update TextComponent.js

--- reactcourse/src/components/TextComponent.js
+++ reactcourse/src/components/TextComponent.js
@@ -3,10 +3,10 @@ import React from 'react';
 class TextComponent extends React.Component {
     render() {
       return(
-        <React.Fragment>
+        <>
             <label htmlFor={'input' + this.props.name}>{this.props.descprition + ':'}</label>
             <input placeholder={this.props.descprition} id={'input' + this.props.name}></input>
-        </React.Fragment>
+        </>
       )
     }
   }
