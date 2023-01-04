40d326c230b77694b74f03484f3b1f73b45cb058
AndrewKhdot <62639706+AndrewKhdot@users.noreply.github.com>
2023-01-04 18:27:24 +0300

    Update App.js

--- reactcourse/src/App.js
+++ reactcourse/src/App.js
@@ -17,12 +17,12 @@ function App() {
   useEffect(() => console.log('hi'), [user])
   return (
     <div className="App">
-    <Header />
+      <Header />
       <div>
-          <Routes>
-            <Route exact path='/' element = {<EnterForm />}></Route>
-            <Route exact path='/active' element = {<ActiveTasks number = {tasks}/>}></Route>
-          </Routes>
+        <Routes>
+          <Route exact path='/' element = {<EnterForm />}></Route>
+          <Route exact path='/active' element = {<ActiveTasks number = {tasks}/>}></Route>
+         </Routes>
       </div>
     </div>
   );
